24a56dce346124c97388066d04070f60a8bbfb2c
Xavier Sala <utrescu@gmail.com>
2024-11-28 22:11:35 +0100

    feat: Definició de les variables d'entrada

---
+++ Vaques/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Vaques;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Definir les variables d'entrada
+        var camio = new Camio(700);
+
+        List<Vaca> vaques = new List<Vaca>()
+        {
+            new Vaca("Maria", 360, 40),
+            new Vaca("Pepa", 250, 35),
+            new Vaca("Flor", 400, 43),
+            new Vaca("Toñi", 180, 28),
+            new Vaca("Conxita", 50, 12),
+            new Vaca("Blanca", 90, 13),
+        };
+        
+        // Variables per calcular
+
+
+        // El programa
+
+        
+        
+    }
+}
